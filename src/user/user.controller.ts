528afd9d38eb571417216d67383985012e6b333d
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-09-13 16:57:54 +0100

    Refactor(user.controller.ts) added @Public decorator to create method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
