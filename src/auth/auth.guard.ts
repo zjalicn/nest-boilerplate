7ec01d6634d2c40e667408e99f9c8ac67af43433
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-09-13 03:08:28 +0100

    Refactor auth.guard.ts to handle JWT token authentication

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,8 +3,11 @@ import {
   ExecutionContext,
   Injectable,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,6 +18,27 @@ export class AuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     this.logger.log(AuthGuard.name);
     const request = context.switchToHttp().getRequest();
-    return true;
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      this.logger.warn('No authorization header');
+      throw new UnauthorizedException('No authorization header');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      this.logger.warn('No token found');
+      throw new UnauthorizedException('No token found');
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+      request.user = decoded;
+      return true;
+    } catch (error) {
+      this.logger.error('Invalid token', error.message);
+      throw new UnauthorizedException('Invalid token');
+    }
   }
 }
